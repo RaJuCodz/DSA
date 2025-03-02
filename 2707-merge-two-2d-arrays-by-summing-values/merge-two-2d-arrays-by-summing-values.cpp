561d649d45297b4288864f61be13affa52bf20aa
Raju Gupta <itsraajjjuuuu@gmail.com>
2025-03-02 20:12:40 +0530

    Time: 1 ms (45.39%) | Memory: 15.6 MB (7.08%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         map<int,int> m;
-        int maxi=0;
         for(auto x : nums1){
             m[x[0]]+=x[1];
-            maxi=max(x[0],maxi);
         }
         for(auto x : nums2){
             m[x[0]]+=x[1];
-             maxi=max(x[0],maxi);
         }
         vector<vector<int>> ans;
         for(auto x :m){
