cc2e318ca56a8cd8dbfa9fc9f7f3ace52922499c
Raju Gupta <itsraajjjuuuu@gmail.com>
2025-03-15 13:53:14 +0530

    Time: 2450 ms (5.03%) | Memory: 509.7 MB (5.03%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<int> a;
     int x;
-    int dp[100001][2];
-    int rec(int i,int last){
+    // int dp[100001][2];
+    int rec(int i,int last,vector<vector<int>> &dp){
         if(i==a.size()) return 0;
         if(dp[i][last]!=-1) return dp[i][last];
         int ans =0;
-        ans=max(ans,rec(i+1,0));
-        if(last==0 && a[i]<=x) ans=max(ans,1+rec(i+1,1));
+        ans=max(ans,rec(i+1,0,dp));
+        if(last==0 && a[i]<=x) ans=max(ans,1+rec(i+1,1,dp));
         return dp[i][last]=ans;
     }
     int minCapability(vector<int>& nums, int k) {
         a=nums;
-        int L =0;
+        int L =0; int n = a.size();
         int R =*max_element(nums.begin(),nums.end());
         int ans =1e9;
         auto check =[&](int y){
             x=y;
-            memset(dp,-1,sizeof(dp));
-            if(rec(0,0)>=k) return true;
+            vector<vector<int>> dp(n+1,vector<int>(2,-1));
+            if(rec(0,0,dp)>=k) return true;
             return false;
         };  
         while(L<=R){
