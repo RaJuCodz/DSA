59f642433e15eefeb01287d8ad62393ff7457888
Raju Gupta <itsraajjjuuuu@gmail.com>
2025-03-15 13:51:56 +0530

    Time: 174 ms (5.03%) | Memory: 72.4 MB (10.07%) - LeetSync

---
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> a;
+    int x;
+    int dp[100001][2];
+    int rec(int i,int last){
+        if(i==a.size()) return 0;
+        if(dp[i][last]!=-1) return dp[i][last];
+        int ans =0;
+        ans=max(ans,rec(i+1,0));
+        if(last==0 && a[i]<=x) ans=max(ans,1+rec(i+1,1));
+        return dp[i][last]=ans;
+    }
+    int minCapability(vector<int>& nums, int k) {
+        a=nums;
+        int L =0;
+        int R =*max_element(nums.begin(),nums.end());
+        int ans =1e9;
+        auto check =[&](int y){
+            x=y;
+            memset(dp,-1,sizeof(dp));
+            if(rec(0,0)>=k) return true;
+            return false;
+        };  
+        while(L<=R){
+            int mid = (L+R)/2;
+            if(check(mid)){
+                R=mid-1;
+                ans=mid;
+            }
+            else L=mid+1;
+        }
+        return ans;
+    }
+};
