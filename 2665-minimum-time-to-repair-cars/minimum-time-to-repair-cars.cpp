f2afc7519fd5291e86829ee6c50a7e71ae4904ec
Raju Gupta <itsraajjjuuuu@gmail.com>
2025-03-16 07:52:44 +0530

    Time: 175 ms (5.09%) | Memory: 60.5 MB (5.08%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long ans =0;
+        long long L =1; long long R = 1e17;
+        auto f =[&](long long mid){
+            long long car =0;
+            for(auto x : ranks){
+                car+=(int)sqrtl(mid/x);
+            }
+            return car>=cars;
+        };
+        while(L<=R){
+            long long mid = (L+R)/2;
+            if(f(mid)){
+                ans=mid;
+                R=mid-1;
+            }
+            else L=mid+1;
+        }
+        return ans;
+    }
+};
